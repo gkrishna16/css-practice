eb564e4a9bb3c26dc25570f0cd9247ec402d4b47
Gopal Krishan <70444883+gkrishna16@users.noreply.github.com>
2022-07-25 21:33:17 -0400

    click not working

---
+++ app.js
@@ -0,0 +1,12 @@
+// let h1add = document.getElementsByClassName(".main-page");
+
+// h1add.addEventListener("click", () => {
+//   console.log(`added event listener`);
+// });
+
+let click = document.getElementsByClassName("click");
+
+click.addEventListener(`click`, () => {
+  alert(`click`);
+  
+});
