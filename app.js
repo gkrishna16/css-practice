d218f73137fe7b67c641220c524e6415433902c8
Gopal Krishan <70444883+gkrishna16@users.noreply.github.com>
2022-07-28 18:40:27 -0400

    added position

--- app.js
+++ app.js
@@ -11,8 +11,28 @@
 
 // });
 
-function add() {
-  return 10 + 100;
-}
+// function add() {
+//   return 10 + 100;
+// }
 
-console.log(add());
+// console.log(add());
+
+// const array1 = [
+//   { name: "kon kiska baaap", year: 2005 },
+//   { name: "andaz apna tumaha", year: 2010 },
+//   { name: "Avengers", year: 2022 },
+//   { name: "Thor", year: 2023 },
+// ];
+
+// const movies = array1.filter((i) => {
+// if (i.name.charAt(0) == "a" || i.name.charAt(0) == "A") return i;
+// });
+
+// const movies = array1.filter(
+//   (f) => f.name.startsWith("A") || f.name.startsWith("a")
+// );
+
+// console.log(movies);
+
+
+
